768e7386395186d9fe0295adadbe2b4029ef3c78
cedrata <81557163+cedrata@users.noreply.github.com>
2023-07-31 15:54:38 +0200

    [ADD] client with placeholder to prevent closing

---
+++ client/src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+    import {preventTabClose} from "$lib/prevetClose";
+
+    let shouldWork = true;
+</script>
+
+<button on:click={() => shouldWork = !shouldWork}>
+	change: {shouldWork}
+</button>
+
+<div use:preventTabClose={shouldWork}>
+	Here would go a huge form that user doesn't want to lose in case of an accident.
+</div>
+
+<!-- {#if connection}
+	connected
+{:else}
+	<button on:click={() => connection = !connection}>connect</button>
+{/if} -->
