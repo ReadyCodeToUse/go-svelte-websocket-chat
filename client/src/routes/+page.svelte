60c4ac48c552068e9cce27cb074b8352203038f8
cedrata <greggius9891@gmail.com>
2023-08-02 12:15:36 +0200

    [ADD] tailwind+daisyui

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -12,6 +12,8 @@
 	Here would go a huge form that user doesn't want to lose in case of an accident.
 </div>
 
+<h1 class="text-lg font-bold underline">Hello world!</h1>
+
 <!-- {#if connection}
 	connected
 {:else}
