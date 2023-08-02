b0ecba188097eed4c0a958a17ee68b456b15b8e4
cedrata <81557163+cedrata@users.noreply.github.com>
2023-08-02 22:38:14 +0200

    [ADD] chat list placeholder
    check if the radio box is a good solution or not

---
+++ client/src/lib/components/preview.svelte
@@ -0,0 +1,6 @@
+<script>
+	/** @type {string} */
+	export let title;
+</script>
+
+<div class="w-full h-20 p-8 bg-neutral hover:bg-neutral-focus divide-solid">{title}</div>
--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,3 +1,13 @@
+<script>
+	import Preview from '$lib/components/preview.svelte';
+
+	const titles = ['preview 1', 'preview 2', 'preview 3', 'preview 4'];
+
+	/** @type {string}*/
+	let preview = '';
+
+</script>
+
 <!-- <script>
     import {preventTabClose} from "$lib/prevetClose";
 
@@ -12,7 +22,15 @@
 	Here would go a huge form that user doesn't want to lose in case of an accident.
 </div> -->
 
-<h1 class="text-lg font-bold underline">Hello world!</h1>
+<h1 class="text-lg font-bold h-10">Chat application</h1>
+<main class="grid grid-cols-10">
+	<div class="col-span-2 h-screen bg-purple-300">
+		{#each titles as title}
+			<input type="radio" name="preview" value={title} bind:group={preview}>
+			<Preview {title}/>
+		{/each}
+	</div>
+</main>
 
 <!-- {#if connection}
 	connected
